7ed5395d9742b3aaeb48984b76f9565f2f02ed9d
ValentineGrachyov <valentinegrachyov@gmail.com>
2021-11-24 14:38:12 +0300

    Commit
    Massive Homework

---
+++ Massive_Hw/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive_Hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] number = new int[2, 3] { { 1, 2, 3 }, { 4,5,6 } };
+            
+            void Min(int[,] input)
+            {
+                int min = int.MaxValue;
+                foreach(int i in input )
+                {
+                    if (i < min)
+                        min = i;
+                }
+
+                Console.WriteLine(min);
+            }
+
+            Min(number);
+            Console.ReadLine();
+           
+        
+        }
+    }
+}
