e4083218070af04024c102119b7e863479f977de
ValentineGrachyov <valentinegrachyov@gmail.com>
2021-11-24 12:13:57 +0300

    Commit

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        static void Main()
+        {
+            var a = new Vektor(10, 11);
+            var b = new Vektor(1, 9);
+            var c = new Vektor(10, 1);
+            bool is_equals = a.Equal_of_Vectors(c);
+            a = a.Add(a);
+            b = b.Mult(b, 3);
+            a.Vektor_ToString();
+            b.Vektor_ToString();
+            a.Cosinus(a);
+            double sc_m = a.Scalar_Mult(a);
+            Console.WriteLine($"{is_equals} {sc_m}") ;
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/RationallFractions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class RationallFractions
+    {
+        private int a;
+        private int b;
+        public RationallFractions()
+        {
+            a = 0;
+            b = 1;
+        }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="x" числитель></param>
+        /// <param name="y" знаменатель></param>
+        public RationallFractions(int x, int y)
+        {
+            a = x;
+            b = y;
+
+        }
+
+        private void reduce_Yourself()
+        {
+            
+            int z = NOD(a, b);
+            a = a / z;
+            b = b / z;
+        }
+
+        private RationallFractions reduce(RationallFractions r)
+        {
+            int z = NOD(r.a, r.b);
+            return new RationallFractions(r.a / z, r.b/z);
+        }
+        private int NOD(int x, int y)
+        {
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+            while (x != 0 && y != 0)
+            {
+                if (x > y)
+
+                    x = x % y;
+                else
+                    y = y % x;
+            }
+            return x + y;
+        }
+        private int Nok(int x, int y)
+        {
+            return x * y / NOD(x, y);
+        }
+
+        public RationallFractions Add(RationallFractions r)
+        {
+            int z = Nok(r.b, b);
+            int z1 = z / b * a + z / r.b * r.a;
+            RationallFractions newZ = new RationallFractions(z1, z);
+            return reduce(newZ);
+
+            
+        }
+        public void Add_Toyourself(RationallFractions r)
+        {
+            int z = Nok(r.b, b);
+            a = z / b * a + z / r.b * r.a;
+            b = z;
+            reduce_Yourself();
+        }
+
+        public RationallFractions Sub(RationallFractions r)
+        {
+            int z = Nok(r.b, b);
+            int z1 = z / b * a + z / r.b * r.a;
+            RationallFractions newZ = new RationallFractions(z1, z);
+        }
+    }
+}
