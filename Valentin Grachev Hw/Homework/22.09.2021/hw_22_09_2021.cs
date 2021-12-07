aa4675a93cf7888fa5a7d5fe2ab458fba25d8d0c
ValentineGrachyov <valentinegrachyov@gmail.com>
2021-12-07 23:28:31 +0300

    .

---
+++ Valentin Grachev Hw/Homework/22.09.2021/hw_22_09_2021.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class hw_22_09_2021
+    {
+        public void Task_1()
+        {
+            Console.WriteLine("Введите количество элементов в массиве");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] a = new int[n];
+
+            for (int i = 0; i < n ; i++)
+            {
+                a[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            int min = Int32.MaxValue;
+
+            foreach(var el in a)
+            {
+                min = Math.Min(min, el);
+            }
+            Console.WriteLine(min);
+
+        }
+        public void Task_2A()
+        {
+            string s = Console.ReadLine();
+            while(s.Length>36)
+            {
+                Console.WriteLine("Введена строка неверной длины");
+                s = Console.ReadLine();
+                
+            }
+
+            char[] chars = s.ToCharArray();
+            foreach(var el in chars)
+            {
+                Console.Write(el);
+            }
+
+        }
+        public void Task_2B()
+        {
+            var x = Int32.Parse(Console.ReadLine());
+
+            var y = Int32.Parse(Console.ReadLine());
+
+            int[][] spiralArray = new int[y][];
+            for (int i = 0; i < y; i++)
+            {
+                spiralArray[i] = new int[x];
+            }
+
+            int l = 0;
+            int k = 0;
+
+            while (l < y)
+            {
+                while (k < x)
+                {
+                    spiralArray[l][k] = Int32.Parse(Console.ReadLine());
+                    k++;
+                }
+
+                k--;
+                l++;
+
+                if (l >= y)
+                {
+                    break;
+                }
+
+                while (k >= 0)
+                {
+                    spiralArray[l][k] = Int32.Parse(Console.ReadLine());
+                    k--;
+                }
+
+                l++;
+                k++;
+            }
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write(spiralArray[i][j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Valentin Grachev Hw/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static  void Main()
+        {
+            var a = new hw_22_09_2021();
+            //a.Task_2B();
+            //a.Task_1();
+            //a.Task_2A();
+
+            var b = new hw_29_09_2021();
+
+            //b.Task_1();
+            //b.Fibbonachi(10);
+
+            //var c = new Circle(4);
+            //c.Print();
+            //var d = new Circle(14);
+            //var e = new Circle(0);
+            //e.Print();
+
+            //var f = new RationalFraction(4, 5);
+            //var g = new RationalFraction(2, 1);
+
+            //f.Mult(g);
+
+            //f.Div(g);
+            //Console.WriteLine(f.ToDouble();
+            //f.Print();
+
+            //var m = new Matrix_2x2(1, 2, 3, 4);
+            //var m1 = new Matrix_2x2(1, 2, 3, 4);
+            //m.Mult(m1);
+            //m.Print();
+
+
+            var s = new Student();
+            s.Read(2000, 7, 19, 70);
+            s.Read(1978, 1, 29, 56);
+            s.Read(2055, 12, 9, 20);
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
