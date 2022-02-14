c1ac1fae3b68329e79ac2acb08d190e0104083f9
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-02-14 23:14:49 +0300

    Create Class1.cs

---
+++ Valentin Grachev Hw/2 Semester Hw/HW_1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework._2_Semester_Hw.HW_1
+{
+    class Class1
+    {
+    }
+}
